d84f8c0617a9d693fbb11c462b4c9213e0891469
vermosen <vermosen.jm@free.fr>
2018-11-10 19:44:23 -0500

    hello world

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#include <Eigen/Core>
+
+int main() {
+
+    int size = 50;
+    // VectorXf is a vector of floats, with dynamic size.
+    Eigen::VectorXf u(size), v(size), w(size);
+    u = v + w;
+
+    exit(EXIT_SUCCESS);
+}
